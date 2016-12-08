060e67218b32a6b56b334ea2e01d1d7868701a85
Hadas Korn <hadas.yarhi@gmail.com>
2016-12-09 00:22:33 +0200

    help message in case of wrong URL

--- index.js
+++ index.js
@@ -10,8 +10,22 @@ function handleRequest(request, response){
  	var array = require('url').parse(request.url,true);
  	var answer = null;
  	if (array["pathname"] == '/numbers'){
- 		var n = array["query"] ["n"];
- 		answer = numbers(n, response);
+		var n = array["query"] ["n"];
+ 		if(n != null){
+ 			answer = numbers(n, response);
+ 		}
+ 	}
+ 	if (answer==null){
+ 		answer = "Expects URLs of the type \nhttp://localhost:3000/numbers?n=37 \nto return "+
+ 		"the numbers from 1 to n , and whenever a number is divided by ";
+ 		SUBSTITUTION.forEach(function (value){
+ 			answer += value[0] + ", ";
+		});
+
+ 		answer += "then substitute it with words representing this number, as Node for 3 and more specificaly : \n";
+ 		SUBSTITUTION.forEach(function (value){
+ 			answer += value[0] + " --> "+ value[1]+ "\n";
+		});
  	}
     response.end(answer);
    
@@ -31,7 +45,6 @@ function numbers(number, response){
 	for (var i = 1; i <= number; i++){
 		ret += substitution(i) + '\n';
 	}
-	// return "wowow i got a number "+number;
 	return ret;
 }
 
@@ -39,12 +52,9 @@ function substitution(number){
 	var sub = "";
 	SUBSTITUTION.forEach(function (value){
 		var first = value[0];
-		// console.log("current "+ current + " first :" + first);	
-		// console.log(current % first === 0);
 		if(number % first === 0){
 			sub += value[1];
 		}
-		// console.log(sub);
 	});
 	if(sub == ""){
 		sub = number;
